3cea259ef434be6b3001ca7bd9347669ff6b01d6
osalvatierra <oscar@owebdesigns.com>
2022-08-31 21:13:58 -0400

    adding missing files again, need to correct

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import CategoryItem from "./components/category-item/category-item.component";
-
-import "./categories.styles.scss";
-
+import Directory from "./components/directory/dir.component.jsx";
 const App = () => {
   const categories = [
     {
@@ -31,13 +28,7 @@ const App = () => {
     },
   ];
 
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;

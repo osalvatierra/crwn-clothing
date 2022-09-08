fdb1a5fa99ac281305a75a0a9cc2ebd2af2fda54
osalvatierra <oscar@owebdesigns.com>
2022-09-07 20:53:24 -0400

    navigation, outlet, link, from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./routes/home/home.component.jsx";
+import Home from "./routes/home/home.component";
+
+import Navigation from './routes/navigation/navigation.component';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -9,8 +11,10 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
-        <Route path="shop" element={<Shop />} />
+      <Route path='/' element={<Navigation/> }>
+
+         <Route index element={<Home />}/>
+         <Route path="shop" element={<Shop />}/>
       </Route>
     </Routes>
   );

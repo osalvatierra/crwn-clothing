e2646ba38432d4d64fc9b0cab753e37a1155956b
osalvatierra <oscar@owebdesigns.com>
2022-10-19 14:44:39 -0400

    adding cart Context and Shopping Icon as well as cart dropdown

---
+++ src/contexts/products.context.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState, useEffect } from "react";
+
+import PRODUCTS from "../shop-data.json";
+
+import {
+	onAuthStateChangedListener,
+	createUserDocumentFromAuth,
+} from "../utils/firebase/firebase.utils";
+
+// as the actual value you want to access
+export const ProductsContext = createContext({
+	products: [],
+});
+
+export const ProductsProvider = ({ children }) => {
+	const [products, setProducts] = useState(PRODUCTS);
+	const value = { products };
+
+	// useEffect(() => {
+	// 	const unsubscribe = onAuthStateChangedListener((user) => {
+	// 		if (user) {
+	// 			createUserDocumentFromAuth(user);
+	// 		}
+	// 		setCurrentUser(user);
+	// 	});
+	// 	return unsubscribe;
+	// }, []);
+
+	return (
+		<ProductsContext.Provider value={value}>
+			{children}
+		</ProductsContext.Provider>
+	);
+};

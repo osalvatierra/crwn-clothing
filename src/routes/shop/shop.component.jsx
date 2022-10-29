11b6db1fa747d9a245d5762bd0ac87c64938ff25
osalvatierra <oscar@owebdesigns.com>
2022-10-29 14:46:48 -0400

    added in our addToCart Feature

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -6,6 +6,8 @@ import ProductCard from "../../components/product-card/product-card.component";
 
 import "./shop.styles.scss";
 
+// products contains all products from Json. Uses useContext w/ ProductsContext import
+// Then ProductCard import, used to add selected products to cart area
 const Shop = () => {
 	const { products } = useContext(ProductsContext);
 	return (
